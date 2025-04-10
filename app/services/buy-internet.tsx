5d01b0914d98787c4b9c006476de43084f0b8c24
Ulephraim <iphraimunlimited@gmail.com>
2025-04-10 12:51:32 +0100

    Improve internet and more services component UI

--- app/services/buy-internet.tsx
+++ app/services/buy-internet.tsx
@@ -7,86 +7,189 @@ import {
   Image,
   TouchableOpacity,
   Switch,
+  Modal,
+  ScrollView,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRouter } from 'expo-router';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import icons from '../../constants/icons';
+import images from '@/constants/images';
 
-const BuyInternet = () => {
+type InternetPackage = {
+  label: string;
+  price: string;
+};
+
+type ProviderKey = 'smile' | 'spectranet';
+
+const InternetScreen = () => {
+  const router = useRouter();
   const [isBeneficiary, setIsBeneficiary] = useState(false);
+  const [selectedProvider, setSelectedProvider] = useState<ProviderKey | null>(
+    null
+  );
+  const [isPackageModalVisible, setIsPackageModalVisible] = useState(false);
+  const [packages, setPackages] = useState<InternetPackage[]>([]);
+  const [selectedPackage, setSelectedPackage] =
+    useState<InternetPackage | null>(null);
+
+  const providerPackages: Record<ProviderKey, InternetPackage[]> = {
+    smile: [
+      { label: 'Buy Airtime', price: '₦93.00' },
+      { label: '1GB FlexiDaily for 1days', price: '₦128.00' },
+      { label: '2.5GB FlexiDaily for 2days', price: '₦350.00' },
+      { label: '1.5GB Bigga for 30days', price: '₦500.00' },
+      { label: '2GB FlexiWeekly for 7days', price: '₦1000.00' },
+    ],
+    spectranet: [
+      { label: 'Buy Airtime', price: '₦903.00' },
+      { label: '1GB FlexiDaily for 1days', price: '₦1298.00' },
+      { label: '2.5GB FlexiDaily for 2days', price: '₦9350.00' },
+      { label: '1.5GB Bigga for 30days', price: '₦5800.00' },
+      { label: '2GB FlexiWeekly for 7days', price: '₦18000.00' },
+    ],
+  };
+
+  const handleProviderSelect = (provider: ProviderKey) => {
+    setSelectedProvider(provider);
+    setPackages(providerPackages[provider]);
+  };
+
+  const providerLogos: Record<ProviderKey, any> = {
+    smile: images.spectranetLogo,
+    spectranet: images.spectranetLogo,
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-[#F5F5F5] px-4">
       {/* Header */}
       <View className="flex-row items-center mt-4 pb-4">
-        <TouchableOpacity>
+        <TouchableOpacity
+          className="w-10"
+          onPress={() => router.push('/(tabs)/home')}
+        >
           <Image source={icons.back} className="w-6 h-6" />
         </TouchableOpacity>
-        <Text className="text-black text-lg font-bold ml-4">Airtime</Text>
-      </View>
-
-      {/* Tab Selector */}
-      <View className="flex-row bg-gray-200 p-2 rounded-lg">
-        <TouchableOpacity className="flex-1 bg-white p-3 rounded-lg">
-          <Text className="text-black text-center">Local</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="flex-1 p-3 rounded-lg">
-          <Text className="text-gray-500 text-center">International</Text>
-        </TouchableOpacity>
+        <View className="flex-1 items-center -ml-10">
+          <Text className="text-black text-lg font-bold">Internet</Text>
+        </View>
       </View>
 
       {/* Select Provider */}
-      <Text className="text-black text-lg font-bold mt-6">
-        Select Service Provider
-      </Text>
-      <View className="flex-row justify-between bg-white p-4 rounded-lg mt-2">
-        <Image source={icons.profile} className="w-14 h-14" />
-        <Image source={icons.profile} className="w-14 h-14" />
-        <Image source={icons.profile} className="w-14 h-14" />
-        <Image source={icons.profile} className="w-14 h-14" />
-      </View>
+      <KeyboardAwareScrollView>
+        <View className="flex-row gap-2 bg-white p-4 rounded-lg mt-2">
+          {(['smile', 'spectranet'] as ProviderKey[]).map((provider) => (
+            <TouchableOpacity
+              key={provider}
+              onPress={() => handleProviderSelect(provider)}
+            >
+              <View
+                className={`rounded-full p-1 ${
+                  selectedProvider === provider
+                    ? 'border-2 border-[#007BFF]'
+                    : ''
+                }`}
+              >
+                <Image
+                  source={providerLogos[provider]}
+                  className="w-14 h-14 rounded-full"
+                />
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
 
-      {/* Phone Number Input */}
-      <Text className="text-black text-lg font-bold mt-6">Phone Number</Text>
-      <View className="bg-white p-3 rounded-lg flex-row items-center mt-2">
-        <TextInput
-          className="flex-1 text-black text-lg"
-          placeholder="Enter phone number"
-        />
-        <Image source={icons.profile} className="w-6 h-6 text-gray-400" />
-      </View>
+        <Text className="text-black text-sm  mt-6">ACCOUNT ID / User ID</Text>
+        <View className="bg-white p-3 rounded-lg flex-row items-center mt-2">
+          <TextInput
+            className="flex-1 text-black text-lg ml-2"
+            keyboardType="numeric"
+          />
+        </View>
 
-      {/* Amount Input */}
-      <Text className="text-black text-lg font-bold mt-6">Amount</Text>
-      <View className="bg-white p-3 rounded-lg flex-row items-center mt-2">
-        <Text className="text-black text-lg">₦</Text>
-        <TextInput
-          className="flex-1 text-black text-lg ml-2"
-          placeholder="0.00"
-          keyboardType="numeric"
-        />
-      </View>
+        {/* Select Data Package */}
+        <Text className="text-black text-lg  mt-6">Select Package</Text>
+        <TouchableOpacity
+          onPress={() => setIsPackageModalVisible(true)}
+          className="bg-white p-4 rounded-lg mt-2"
+        >
+          <Text className="text-black text-base">
+            {selectedPackage
+              ? selectedPackage.label
+              : 'Tap to select a package'}
+          </Text>
+        </TouchableOpacity>
 
-      {/* Save as Beneficiary */}
-      <View className="bg-white p-4 rounded-lg mt-6 flex-row justify-between items-center">
-        <Text className="text-black text-lg font-bold">
-          Save as Beneficiary
-        </Text>
-        <Switch
-          value={isBeneficiary}
-          onValueChange={setIsBeneficiary}
-          trackColor={{ true: '#007BFF', false: '#ccc' }}
-        />
-      </View>
+        {/* Modal */}
+        <Modal visible={isPackageModalVisible} transparent animationType="fade">
+          <TouchableWithoutFeedback
+            onPress={() => setIsPackageModalVisible(false)}
+          >
+            <View className="flex-1 justify-end bg-black/50">
+              <TouchableWithoutFeedback>
+                <View className="bg-white rounded-t-2xl p-4 max-h-[70%]">
+                  <View className="items-center mb-4">
+                    <View className="w-10 h-1 bg-black rounded-full" />
+                    <Text className="text-black font-bold text-lg mt-2">
+                      Select Package
+                    </Text>
+                  </View>
+                  <ScrollView>
+                    {packages.map((item, index) => (
+                      <TouchableOpacity
+                        key={index}
+                        className="flex-row justify-between items-center p-4 border-b border-gray-200"
+                        onPress={() => {
+                          setSelectedPackage(item);
+                          setIsPackageModalVisible(false);
+                        }}
+                      >
+                        <Text className="text-black">{item.label}</Text>
+                        <Text className="text-black">{item.price}</Text>
+                      </TouchableOpacity>
+                    ))}
+                  </ScrollView>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        </Modal>
 
-      {/* Continue Button */}
-      <TouchableOpacity className="bg-[#007BFF] p-4 rounded-lg mt-6">
-        <Text className="text-white text-center text-lg font-bold">
-          Continue
-        </Text>
-      </TouchableOpacity>
+        {/* Amount Input */}
+        <Text className="text-black text-lg  mt-6">Amount</Text>
+        <View className="bg-white p-3 rounded-lg flex-row items-center mt-2">
+          <Text className="text-black text-lg">₦</Text>
+          <TextInput
+            className="flex-1 text-black text-lg ml-2"
+            placeholder="0.00"
+            keyboardType="numeric"
+          />
+        </View>
+
+        {/* Save Beneficiary */}
+        <View className="bg-white p-4 rounded-lg mt-6 flex-row justify-between items-center">
+          <Text className="text-black text-lg font-bold">
+            Save as Beneficiary
+          </Text>
+          <Switch
+            value={isBeneficiary}
+            onValueChange={setIsBeneficiary}
+            trackColor={{ true: '#007BFF', false: '#ccc' }}
+          />
+        </View>
+
+        {/* Continue Button */}
+        <TouchableOpacity className="bg-[#007BFF] p-4 rounded-lg mt-6">
+          <Text className="text-white text-center text-lg font-bold">
+            Continue
+          </Text>
+        </TouchableOpacity>
+      </KeyboardAwareScrollView>
     </SafeAreaView>
   );
 };
 
-export default BuyInternet;
+export default InternetScreen;
--- app/services/more-services.tsx
+++ app/services/more-services.tsx
@@ -1,90 +1,103 @@
 /** @format */
 
-import {
-  View,
-  Text,
-  TextInput,
-  Image,
-  TouchableOpacity,
-  Switch,
-} from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import icons from '../../constants/icons';
+import { useRouter } from 'expo-router';
+import ServiceItem from '@/components/ServiceItem';
+
+const services: ServiceItem[] = [
+  {
+    id: 1,
+    name: 'Airtime',
+    icon: icons.telephone,
+    route: '/services/buy-airtime',
+  },
+  {
+    id: 2,
+    name: 'Data',
+    icon: icons.data,
+    route: '/services/buy-data',
+  },
+  {
+    id: 3,
+    name: 'Electricity',
+    icon: icons.electricity,
+    route: '/services/buy-electricity',
+  },
+  {
+    id: 4,
+    name: 'TV',
+    icon: icons.television,
+    route: '/services/buy-tv',
+  },
+  {
+    id: 5,
+    name: 'Internet',
+    icon: icons.internet,
+    route: '/services/buy-internet',
+  },
+  {
+    id: 6,
+    name: 'ESims',
+    icon: icons.esim,
+    route: '/services/buy-esim',
+  },
+  {
+    id: 7,
+    name: 'Betting',
+    icon: icons.betting,
+    route: '/services/buy-betting',
+  },
+  {
+    id: 8,
+    name: 'More',
+    icon: icons.more,
+    route: '/services/more-services',
+  },
+];
+
+const billsServices = services.slice(0, 4); // First four
+const lifestyleServices = services.slice(4); // Next four
 
 const MoreServices = () => {
-  const [isBeneficiary, setIsBeneficiary] = useState(false);
+  const router = useRouter();
 
   return (
     <SafeAreaView className="flex-1 bg-[#F5F5F5] px-4">
       {/* Header */}
       <View className="flex-row items-center mt-4 pb-4">
-        <TouchableOpacity>
+        <TouchableOpacity
+          className="w-10"
+          onPress={() => router.push('/(tabs)/home')}
+        >
           <Image source={icons.back} className="w-6 h-6" />
         </TouchableOpacity>
-        <Text className="text-black text-lg font-bold ml-4">Airtime</Text>
+        <View className="flex-1 items-center -ml-10">
+          <Text className="text-black text-lg font-bold">All Services</Text>
+        </View>
       </View>
 
-      {/* Tab Selector */}
-      <View className="flex-row bg-gray-200 p-2 rounded-lg">
-        <TouchableOpacity className="flex-1 bg-white p-3 rounded-lg">
-          <Text className="text-black text-center">Local</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="flex-1 p-3 rounded-lg">
-          <Text className="text-gray-500 text-center">International</Text>
-        </TouchableOpacity>
+      {/* Bills Payment Section */}
+      <View className="bg-white p-4 rounded-2xl mt-4">
+        <Text className="text-black text-lg mb-2 ml-4">Bills Payment</Text>
+        <View className="flex-row flex-wrap justify-between">
+          {billsServices.map((item) => (
+            <ServiceItem key={item.id} item={item} />
+          ))}
+        </View>
       </View>
 
-      {/* Select Provider */}
-      <Text className="text-black text-lg font-bold mt-6">
-        Select Service Provider
-      </Text>
-      <View className="flex-row justify-between bg-white p-4 rounded-lg mt-2">
-        <Image source={icons.profile} className="w-14 h-14" />
-        <Image source={icons.profile} className="w-14 h-14" />
-        <Image source={icons.profile} className="w-14 h-14" />
-        <Image source={icons.profile} className="w-14 h-14" />
+      {/* Lifestyle Section */}
+      <View className="bg-white p-4 rounded-2xl mt-4">
+        <Text className="text-black text-lg mb-2 ml-4">Lifestyle</Text>
+        <View className="flex-row flex-wrap justify-between">
+          {lifestyleServices.map((item) => (
+            <ServiceItem key={item.id} item={item} />
+          ))}
+        </View>
       </View>
-
-      {/* Phone Number Input */}
-      <Text className="text-black text-lg font-bold mt-6">Phone Number</Text>
-      <View className="bg-white p-3 rounded-lg flex-row items-center mt-2">
-        <TextInput
-          className="flex-1 text-black text-lg"
-          placeholder="Enter phone number"
-        />
-        <Image source={icons.profile} className="w-6 h-6 text-gray-400" />
-      </View>
-
-      {/* Amount Input */}
-      <Text className="text-black text-lg font-bold mt-6">Amount</Text>
-      <View className="bg-white p-3 rounded-lg flex-row items-center mt-2">
-        <Text className="text-black text-lg">₦</Text>
-        <TextInput
-          className="flex-1 text-black text-lg ml-2"
-          placeholder="0.00"
-          keyboardType="numeric"
-        />
-      </View>
-
-      {/* Save as Beneficiary */}
-      <View className="bg-white p-4 rounded-lg mt-6 flex-row justify-between items-center">
-        <Text className="text-black text-lg font-bold">
-          Save as Beneficiary
-        </Text>
-        <Switch
-          value={isBeneficiary}
-          onValueChange={setIsBeneficiary}
-          trackColor={{ true: '#007BFF', false: '#ccc' }}
-        />
-      </View>
-
-      {/* Continue Button */}
-      <TouchableOpacity className="bg-[#007BFF] p-4 rounded-lg mt-6">
-        <Text className="text-white text-center text-lg font-bold">
-          Continue
-        </Text>
-      </TouchableOpacity>
     </SafeAreaView>
   );
 };
